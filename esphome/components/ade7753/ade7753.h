8d8fe1195ee936cf1913e92c89e94acea242c61f
André Clemente <andreclemente@users.noreply.github.com>
2022-07-07 17:21:39 +0100

    Update ade7753.h

--- esphome/components/ade7753/ade7753.h
+++ esphome/components/ade7753/ade7753.h
@@ -2,13 +2,13 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
-#include "esphome/components/i2c/i2c.h"
+#include "esphome/components/spi/spi.h"
 #include "esphome/components/sensor/sensor.h"
 
 namespace esphome {
 namespace ade7753 {
 
-class ADE7753 : public i2c::I2CDevice, public PollingComponent {
+class ADE7753 : public spi::SPIDevice, public PollingComponent {
  public:
   void set_voltage_sensor(sensor::Sensor *voltage_sensor) { voltage_sensor_ = voltage_sensor; }
   void set_current_a_sensor(sensor::Sensor *current_a_sensor) { current_a_sensor_ = current_a_sensor; }
@@ -31,14 +31,14 @@ class ADE7753 : public i2c::I2CDevice, public PollingComponent {
   void update() override;
 
  protected:
-  i2c::ErrorCode ade_write_8_(uint16_t reg, uint8_t value) {
+  spi::ErrorCode ade_write_8_(uint16_t reg, uint8_t value) {
     std::vector<uint8_t> data;
     data.push_back(reg >> 8);
     data.push_back(reg >> 0);
     data.push_back(value);
     return write(data.data(), data.size());
   }
-  i2c::ErrorCode ade_write_16_(uint16_t reg, uint16_t value) {
+  spi::ErrorCode ade_write_16_(uint16_t reg, uint16_t value) {
     std::vector<uint8_t> data;
     data.push_back(reg >> 8);
     data.push_back(reg >> 0);
@@ -46,7 +46,7 @@ class ADE7753 : public i2c::I2CDevice, public PollingComponent {
     data.push_back(value >> 0);
     return write(data.data(), data.size());
   }
-  i2c::ErrorCode ade_write_32_(uint16_t reg, uint32_t value) {
+  spi::ErrorCode ade_write_32_(uint16_t reg, uint32_t value) {
     std::vector<uint8_t> data;
     data.push_back(reg >> 8);
     data.push_back(reg >> 0);
@@ -56,23 +56,23 @@ class ADE7753 : public i2c::I2CDevice, public PollingComponent {
     data.push_back(value >> 0);
     return write(data.data(), data.size());
   }
-  i2c::ErrorCode ade_read_32_(uint16_t reg, uint32_t *value) {
+  spi::ErrorCode ade_read_32_(uint16_t reg, uint32_t *value) {
     uint8_t reg_data[2];
     reg_data[0] = reg >> 8;
     reg_data[1] = reg >> 0;
-    i2c::ErrorCode err = write(reg_data, 2);
-    if (err != i2c::ERROR_OK)
+    spi::ErrorCode err = write(reg_data, 2);
+    if (err != spi::ERROR_OK)
       return err;
     uint8_t recv[4];
     err = read(recv, 4);
-    if (err != i2c::ERROR_OK)
+    if (err != spi::ERROR_OK)
       return err;
     *value = 0;
     *value |= ((uint32_t) recv[0]) << 24;
     *value |= ((uint32_t) recv[1]) << 16;
     *value |= ((uint32_t) recv[2]) << 8;
     *value |= ((uint32_t) recv[3]);
-    return i2c::ERROR_OK;
+    return spi::ERROR_OK;
   }
 
   bool is_setup_{false};
